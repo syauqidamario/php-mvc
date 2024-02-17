47cbe387d164612963001587b172202f98ec6576
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-02-17 20:00:02 +0700

    Create Models in PHP MVC

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,46 @@
+<?php
+
+
+class HomeController
+{
+
+      function index(): void
+      {
+            $model = [
+                  "title" => "Belajar PHP MVC",
+                  "content" => "Selamat Belajar PHP MVC dari Programmer Zaman Now"
+            ];
+
+            echo "HomeController.index()";
+      }
+
+      function hello(): void
+      {
+            echo "HomeController.hello()";
+      }
+
+      function world(): void
+      {
+            echo "HomeController.world()";
+      }
+
+      function about(): void
+      {
+            echo "Author : Eko Kurniawan Khannedy";
+      }
+
+      function login(): void
+      {
+            $request = [
+                  "username" => $_POST['username'],
+                  "password" => $_POST['password']
+            ];
+
+            $user = [];
+
+            $response = [
+                  "message" => "Login Sukses"
+            ];
+            // kirimkan response ke view
+      }
+}
