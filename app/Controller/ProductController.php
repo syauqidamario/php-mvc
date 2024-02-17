8a56f5ffb765b50579fa066bd9629974147f043c
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-02-17 14:05:55 +0700

    Implemented routing and add some controllers.
    Path variables in PHP MVC.

---
+++ app/Controller/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SyauqiDamarioDjohan\Learn\PHP\MVC\Controller;
+
+class ProductController
+{
+      function categories(string $productId, string $categoryId): void
+      {
+            echo "PRODUCT $productId, CATEGORY $categoryId";
+      }
+}
